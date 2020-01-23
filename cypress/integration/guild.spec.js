2318e314d27ef597e50ee96d223c8b378f13f105
Nathan Phillips <nathanphillips@MacBook-Pro-7.local>
2020-01-23 16:03:15 +0000

    added guild test

---
+++ cypress/integration/guild.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/commands/guild')
+  })
+
+  it('Very simple test', () => {
+  
+  })
+
+  it('click the right element', () => {
+  
+  })
+
+  it('Good selectors', () => {
+  
+  })
+
+  it('Thinking about asynchronicity', () => {
+  
+  })
+
+  it('Negative testing', () => {
+  
+  })
+
+  it('Re-usable code.', () => {
+  
+  })
+
+  it('Using fixtures.', () => {
+  
+  })
+})
